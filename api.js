a9339ebdbf7f7f523534b8657b052196c3d8f7b6
Kasim-Ansari786 <comdata0201@gmail.com>
2025-11-26 00:11:41 +0530

    Update api.js

--- api.js
+++ api.js
@@ -61,7 +61,7 @@ const handleApiCall = async (url, options = {}) => {
 
 export const loginUser = async ({ email, password, role }) => {
   try {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+    const response = await fetch(`${API_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password, role }),
